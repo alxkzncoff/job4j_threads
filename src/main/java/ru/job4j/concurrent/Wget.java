806030a43ce7e0a795d887c1fce50e8d52327bcc
Aleksandr <alx.kzncoff@gmail.com>
2022-03-02 11:33:01 +0300

    1.2. Режим ожидания. [#231217 #270934]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,15 +3,18 @@ package ru.job4j.concurrent;
 public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(
-                () -> { }
+                () -> {
+                    for (int i = 0; i < 100; i++) {
+                        try {
+                            Thread.sleep(100);
+                            System.out.print("\rLoading : " + i  + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("\rLoading : Done.");
+                }
         );
-        for (int i = 0; i < 100; i++) {
-            try {
-                Thread.sleep(1000);
-                System.out.print("\rLoading : " + i  + "%");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        thread.start();
     }
 }
