40d4c727ae50da6233b141ef1530c74b47a745ea
Aleksandr <alx.kzncoff@gmail.com>
2022-03-09 13:23:09 +0300

    4. Скачивание файла с ограничением. [#144271 #271366]

--- src/main/java/ru/job4j/concurrent/thread/Wget.java
+++ src/main/java/ru/job4j/concurrent/thread/Wget.java
@@ -15,42 +15,57 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
-        long startTime;
-        long endTime;
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
-            byte[] dataBuffer = new byte[speed];
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            startTime = System.currentTimeMillis();
-            while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
-                try {
-                    fileOutputStream.write(dataBuffer, 0, bytesRead);
-                    endTime = System.currentTimeMillis();
-                    if (endTime - startTime < 1000) {
-                        Thread.sleep(1000 - (endTime - startTime));
+            long bytesWrite = 0;
+            long startTime = System.currentTimeMillis();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                bytesWrite += bytesRead;
+                if (bytesWrite >= speed) {
+                    bytesWrite = 0;
+                    long deltaTime = System.currentTimeMillis() - startTime;
+                    if (deltaTime < 1000) {
+                        try {
+                            Thread.sleep(1000 - deltaTime);
+                            startTime = System.currentTimeMillis();
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
                     }
-                    startTime = System.currentTimeMillis();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public static void argsValidate(int argsNum) {
+        if (argsNum != 3) {
+            throw new IllegalArgumentException("Parameters missed! Required three params: File path to download, "
+                    + "download speed (byte per second) and file name to save. \n"
+                    + "Example: https://site.com/file.txt 1048576 MyFile.txt");
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        argsValidate(args.length);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
