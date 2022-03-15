a3f630a63d212e6bbad8daeaa3f50b54b568c098
Aleksandr <alx.kzncoff@gmail.com>
2022-03-15 16:07:18 +0300

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/SimpleBlockingQueue.java
@@ -0,0 +1,62 @@
+package ru.job4j;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Реализация шаблона Producer Consumer с использованием wait/notify.
+ * Producer помещает данные в очередь. Consumer извлекает данные из очереди.
+ * Если очередь заполнена полностью, то при попытке добавления поток Producer блокируется,
+ * до тех пор пока Consumer не извлечет очередные данные.
+ * И наоборот если очередь пуста поток Consumer блокируется,
+ * до тех пор пока Producer не поместит в очередь данные.
+ *
+ * @author Aleksandr Kuznetsov.
+ * @version 1.0
+ */
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private final Queue<T> queue = new LinkedList<>();
+    private final int capacity;
+
+    public SimpleBlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    /**
+     * Метод помещает данные в очередь, если она не полностью заполнена,
+     * иначе поток блокируется, пока не освободится место.
+     * @param value
+     */
+    public void offer(T value) throws InterruptedException {
+        synchronized (this) {
+            while (queue.size() == capacity) {
+                wait();
+            }
+            queue.offer(value);
+            notifyAll();
+        }
+    }
+
+    /**
+     * Метод забирает данные из очереди, если она не пуста,
+     * иначе поток блокируется, пока в очередь не будут
+     * помещены новые данные.
+     * @return T объект очереди.
+     */
+    public T poll() throws InterruptedException {
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                wait();
+            }
+            T value  = queue.poll();
+            notifyAll();
+            return value;
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -0,0 +1,41 @@
+package ru.job4j;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void offerThenPoll() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(2);
+        List<Integer> result = new ArrayList<>();
+        Thread offerTread = new Thread(() -> {
+            for (int i = 1; i <= 6; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread poolTread = new Thread(() -> {
+            for (int i = 1; i <= 6; i++) {
+                try {
+                    result.add(queue.poll());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        offerTread.start();
+        poolTread.start();
+        offerTread.join();
+        poolTread.join();
+        assertThat(result, is(List.of(1, 2, 3, 4, 5, 6)));
+    }
+}
